8f07e7895b543436a6b36f82d58841298516b618
TomiukQT <TomiukQT@gmail.com>
2021-05-27 11:52:13 +0200

    Add simple test

---
+++ Assets/Tests/new_test.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class new_test
+{
+    
+    [Test]
+    public void testing_tests()
+    {
+        Assert.AreEqual(1,1);
+    }
+
+}
