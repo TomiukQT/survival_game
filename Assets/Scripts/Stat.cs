9342567c74780149fed3dd618822b2a5c6e03469
Tomáš Valenta <TomiukQT@gmail.com>
2021-06-20 15:15:23 +0200

    Basic Stat Implementation

---
+++ Assets/Scripts/Stat.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[System.Serializable]
+public class Stat
+{
+    private string _name;
+    private int _value;
+    //private int _initialValue;
+    
+    public Stat(int value, string name = "defautlName")
+    {
+        _value = value;
+        
+        _name = name;
+    }
+
+    public void Increase(int amount = 1)
+    {
+        if (amount < 0)
+            return;
+        _value += amount;
+    }
+
+    public void Decrease(int amount = 1)
+    {
+        if (amount < 0 && _value - amount < 0)
+            return;
+        _value -= amount;
+    }
+
+    public void ChangeStatTemporary(int newValue, float time)
+    {
+        if(newValue < 0 || time < 0f)
+            return;
+    }
+
+    private IEnumerator ChangeStatOnTime(int newValue, float time)
+    {
+        yield return new WaitForSeconds(time);
+    }
+
+    public int Value => _value;
+    public string Name => _name;
+
+}
