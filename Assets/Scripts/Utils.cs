e3bf84e390b0611f4e7d7f98789c46fde918cfa8
Tomáš Valenta <TomiukQT@gmail.com>
2021-06-23 23:08:49 +0200

    Update Utils.cs

--- Assets/Scripts/Utils.cs
+++ Assets/Scripts/Utils.cs
@@ -1,10 +1,21 @@
-﻿public static class Utils
+﻿using UnityEngine;
+
+public static class Utils
 {
 
     
     public static int Fib(int n)
     {
         return n == 1 ? 1 : n == 2 ? 1 : Fib(n - 1) + Fib(n - 2);
+    }
+
+    public static float GetAngleFromVectorFloat(Vector3 dir)
+    {
+        dir = dir.normalized;
+        float n = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        if (n < 0)
+            n += 360;
+        return n;
     }
     
 }
