c2a60f5e60d6838ad3d3f5c2874306ed4710d804
Tomáš Valenta <TomiukQT@gmail.com>
2021-07-01 22:38:20 +0200

    Cooldown filler done

--- Assets/Scripts/UI/UI_SkillBarSlot.cs
+++ Assets/Scripts/UI/UI_SkillBarSlot.cs
@@ -4,14 +4,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using TMPro;
 
 public class UI_SkillBarSlot : MonoBehaviour, IDropHandler
 {
     private Image _image;
+    
+    private Image _cooldownFiller;
+    private TextMeshProUGUI _cooldownText;
 
+    private float _cooldown;
+    
     private void Awake()
     {
         _image = transform.Find("icon").GetComponent<Image>();
+        
+        Transform filler = transform.Find("cooldown_filler");
+        _cooldownFiller = filler.GetComponent<Image>();
+        _cooldownText = filler.GetComponentInChildren<TextMeshProUGUI>();
+    }
+
+    private void Start()
+    {
+        CooldownToggle(false);
     }
 
     public void OnDrop(PointerEventData eventData)
@@ -22,11 +37,31 @@ public class UI_SkillBarSlot : MonoBehaviour, IDropHandler
         Spell spell = eventData.pointerDrag.GetComponent<UI_SkillButton>().Spell;
         _image.sprite = spell.Icon;
         Debug.Log($"Spell {spell.Name} was dropped to slot {gameObject}");
+        OnSpellCast(spell.Cooldown);
     }
 
+    
     public void OnSpellCast(float cooldown)
     {
-        
+        _cooldown = cooldown;
+        CooldownToggle(true);
+        StartCoroutine(CooldownTimer(cooldown));
     }
-    
+
+    private IEnumerator CooldownTimer(float cooldown)
+    {
+        float currTime = cooldown;
+        float STEP_SIZE = 0.1f;
+        while (currTime > 0f)
+        {
+            _cooldownFiller.fillAmount = currTime/cooldown;
+            _cooldownText.text = $"{currTime:F1}";
+            yield return new WaitForSeconds(STEP_SIZE);
+            currTime -= STEP_SIZE;
+        }
+        CooldownToggle(false);
+    }
+
+    private void CooldownToggle(bool state) => _cooldownFiller.gameObject.SetActive(state);
+
 }
